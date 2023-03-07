8b46c934210730252406711c89fe10563e8a7f7e
ggalushko <titanmelchior@gmail.com>
2023-03-07 09:57:05 +0300

    hide addtask input

--- src/AddTask.js
+++ src/AddTask.js
@@ -2,23 +2,25 @@ import { useState } from "react";
 
 const AddTask = ({ addTaskHandler }) => {
   const [inputValue, setInputValue] = useState("");
+  const [inputIsHidden, setInputIsHidden] = useState(false)
+
   return (
-    <form className="add-task"  onSubmit={(e) => {
-      e.preventDefault();
-      inputValue && addTaskHandler(inputValue);
-      setInputValue("")
-    }}>
+    <form
+      className="add-task"
+      onSubmit={(e) => {
+        e.preventDefault();
+        inputValue && addTaskHandler(inputValue);
+        setInputValue("");
+        setInputIsHidden(!inputIsHidden)
+      }}
+    >
       <input
         type="text"
-        className="add-task__input"
+        className={inputIsHidden? "add-task__input visible" : "add-task__input"}
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       ></input>
-      <button
-       
-        type="submit"
-        className="add-task__btn"
-      ></button>
+      <button type="submit" className="add-task__btn"></button>
     </form>
   );
 };
